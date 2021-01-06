21075840b5d7c5036d7a0ea5dba57ced5cc3d306
Tejus <tejus@pop-os.localdomain>
2021-01-06 22:56:03 +0530

    Fix: Send the entire project description

--- commands/looking-for-people.js
+++ commands/looking-for-people.js
@@ -1,13 +1,18 @@
 module.exports = {
 	name: 'looking-for-people',
 	execute(client, message, args) {
-        const general = client.channels.cache.find(channel => channel.id === '796334456272584749')
+        const general = client.channels.cache.find(channel => channel.name === 'general')
         if (message.channel.id != '796382685689937980') {
             console.log("This is not the channel fren")
             return;
         }
         const nickname = message.member.displayName
-        let content =  `Hey! ${nickname} is looking for ${args[0]} people for their project.\nDesc: ${args[1]}.\nReact with :partying_face: if you want to work on it`
-        general.send(content)
+        let projectDesc = ""
+        for(let i = 1; i < args.length; i++)
+            projectDesc+= `${args[i]} `
+        let content =  `Hey! ${nickname} is looking for ${args[0]} people for their project.\nDesc: ${projectDesc}.\nReact with :partying_face: if you want to work on it`
+        general.send(content).then(sent=>{
+            console.log(sent.id)
+        })
 	},
 };
